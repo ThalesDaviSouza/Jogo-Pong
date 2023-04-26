69c5b7e64b5673bd0dd9c7a185164d9ad603bba8
Thales <thalesdavisoul@gmail.com>
2023-04-26 17:36:16 -0300

    Initial Commit

---
+++ Assets/Scripts/ballMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start(){
+        GetComponent<Rigidbody2D>().velocity =
+            Vector2.right * speed;
+    }
+
+    float hitFactor(Vector2 bolaPos, Vector2 jogPos, float tam){
+			
+	    return (bolaPos.y - jogPos.y)/tam;
+    }
+
+    void OnCollisionEnter2D(Collision2D colisao){
+				
+        if (colisao.gameObject.name == "AI"){
+                float y = hitFactor(transform.position, colisao.transform.position, colisao.collider.bounds.size.y);
+
+        Vector2 dir = new Vector2(1,y).normalized;
+    
+        GetComponent<Rigidbody2D>().velocity = dir*speed;
+        }
+
+        if (colisao.gameObject.name == "Player"){
+                float y = hitFactor(transform.position, colisao.transform.position, colisao.collider.bounds.size.y);
+
+        Vector2 dir = new Vector2(-1,y).normalized;
+    
+        GetComponent<Rigidbody2D>().velocity = dir*speed;
+        }
+    }
+}
---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public string axis;
+
+    // Update is called once per frame
+    void Update(){
+        
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = 
+            new Vector2(0,v) * speed;
+
+    }
+}
